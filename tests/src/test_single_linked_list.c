814be6be388bcd9086f1ca4294f77d74b1183ecb
Dimitris Papadimitriou <jim_pap@live.com>
2024-10-21 15:24:31 +0300

    changes needed because of decleration change of sll_node_init in last commit

--- tests/src/test_single_linked_list.c
+++ tests/src/test_single_linked_list.c
@@ -24,23 +24,27 @@ MU_TEST(sll_init_initial_values){
     free(list);
 }
 MU_TEST(sll_node_init_bad_input){
-    int data_size = sizeof(int);
-    SingleLinkedList *list=NULL;
-    SingleLinkedListNode *node = sll_node_init(list);
-    mu_assert(node == NULL, "Should return NULL with NULL input"); 
+    int data_size;
+    data_size = 0;
+    SingleLinkedListNode *node = sll_node_init(data_size);
+    mu_assert(node == NULL, "Should return NULL data_size=0"); 
+    data_size = -1;
+    node = sll_node_init(data_size);
+    mu_assert(node == NULL, "Should return NULL data_size=-1"); 
 }
 MU_TEST(sll_node_init_initial_values){
     int data_size = sizeof(int);
-    SingleLinkedList *list=sll_init(data_size);
-    SingleLinkedListNode *node = sll_node_init(list);
+    SingleLinkedListNode *node = sll_node_init(data_size);
     mu_assert(node->data==NULL,"node->data is not NULL after creation");
     mu_assert(node->next==NULL,"node->next is not null after creation");
+    free(node);
 }
 
 MU_TEST_SUITE(suite_sll){
     MU_SUITE_CONFIGURE(suite_sll_setup,suite_sll_teardown);
     MU_RUN_TEST(sll_init_bad_input);
     MU_RUN_TEST(sll_init_initial_values);
+    MU_RUN_TEST(sll_node_init_bad_input);
     MU_RUN_TEST(sll_node_init_initial_values);
 }
 
