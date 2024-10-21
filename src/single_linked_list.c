0330f02050e1a7811b9f8474bd3731536216c7ce
Dimitris Papadimitriou <jim_pap@live.com>
2024-10-21 15:05:56 +0300

    tests and implementation for initial values in sll_init

--- src/single_linked_list.c
+++ src/single_linked_list.c
@@ -3,4 +3,8 @@
 
 SingleLinkedList *sll_init(int data_size){
     if (data_size <= 0)return NULL;
+    SingleLinkedList * list = malloc(sizeof(SingleLinkedList));
+    list->data_size = data_size;
+    list->head = NULL;
+    return list;
 }
--- tests/src/test_single_linked_list.c
+++ tests/src/test_single_linked_list.c
@@ -16,10 +16,18 @@ MU_TEST(sll_init_bad_input){
     list = sll_init(-1);
     mu_assert(list == NULL,"Not NULL when data_size = -1");
 }
+MU_TEST(sll_init_initial_values){
+    int data_size = sizeof(int);
+    SingleLinkedList *list = sll_init(data_size);
+    mu_assert(list->data_size = data_size,"Bad data_size after init");
+    mu_assert(list->head==NULL,"list->head is not NULL after init");
+
+}
 
 MU_TEST_SUITE(suite_sll){
     MU_SUITE_CONFIGURE(suite_sll_setup,suite_sll_teardown);
     MU_RUN_TEST(sll_init_bad_input);
+    MU_RUN_TEST(sll_init_initial_values);
 }
 
 int main(int argc, char *argv[]) {
