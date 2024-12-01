e1bfa0fb2d50b83376a5c0e1d66510c1423eff87
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-01 23:42:51 +0200

    implemented sll_create()

--- src/single_linked_list.c
+++ src/single_linked_list.c
@@ -1,5 +1,8 @@
 #include "single_linked_list.h"
+#include "stdlib.h"
 
-int add(int a, int b){
-    return a+b;
+SingleLinkedList *sll_create(int data_size){
+    if( data_size<=0 || data_size > SLL_MAX_NODE_DATASIZE_BYTES )return NULL;
+    SingleLinkedList *list = malloc(data_size);
+    return list;
 }
--- src/single_linked_list.h
+++ src/single_linked_list.h
@@ -1,7 +1,24 @@
+#ifndef SINGLE_LINKED_LIST_H
+#define SINGLE_LINKED_LIST_H
+
+#define SLL_MAX_NODE_DATASIZE_BYTES 256
+
 /**
- * A dummy add function
- * @param[in] a First operand
- * @param[in] b Second operand
- * @returns The sum of the two operands
+ * A struct representing a Single Linked List node
  */
-int add(int a, int b);
+typedef struct SLL_Node{
+    struct SLL_Node *next; /** Pointer to the next node */
+    void *data;             /**Pointer to the node data */
+}SLL_Node;
+
+/**
+ * A struct representing a Single Linked List.
+ */
+typedef struct SingleLinkedList{
+    int data_size; /** Size in bytes of the node data field */
+    SLL_Node *head; /** Pointer to the head of the list */
+}SingleLinkedList;
+
+SingleLinkedList *sll_create(int data_size);
+
+#endif
