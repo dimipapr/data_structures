7aed25e9f5cbd6b975aeaad4f89314e283397519
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-06 12:52:22 +0200

    tests and implementation for sllCreate

--- src/single_linked_list.c
+++ src/single_linked_list.c
@@ -1,3 +1,13 @@
 #include "single_linked_list.h"
 #include <stdlib.h>
 #include <string.h>
+
+SingleLinkedList* sllCreate(int data_size){
+    if ( data_size<=0 || data_size>SLL_NODE_DATA_BYTES_MAX )return NULL;
+    SingleLinkedList *list = malloc(sizeof(SingleLinkedList));
+    if(list){
+        list->pHead = NULL;
+        list->dataSize = data_size;
+    }
+    return list;
+}
--- tests/src/test_single_linked_list.c
+++ tests/src/test_single_linked_list.c
@@ -2,7 +2,28 @@
 #include "single_linked_list.h"
 #include <stdlib.h>
 
+//sllCreate
+MU_TEST(test_sllCreate_badInput){
+	SingleLinkedList *list = sllCreate(0);
+	mu_assert(list==NULL,"sllCreate should return NULL with data_size=0");
+	list = sllCreate(-1);
+	mu_assert(list==NULL,"sllCreate should return NULL with data_size=0");
+	list = sllCreate(SLL_NODE_DATA_BYTES_MAX+1);
+	mu_assert(list==NULL,"sllCreate should return NULL with data_size>SLL_NODE_DATA_BYTES_MAX");
+}
+MU_TEST(test_sllCreate_initialValues){
+	int dataSize = sizeof(int);
+	SingleLinkedList *list = sllCreate(dataSize);
+	mu_assert(list, "Unexpected NULL from sllCreate");
+	mu_assert(list->pHead == NULL, "list->pHead should initialize to NULL");
+	mu_assert(list->dataSize == dataSize, "list->dataSize bad initialization");
+}
+MU_TEST_SUITE(suite_sllCreate){
+	MU_RUN_TEST(test_sllCreate_badInput);
+	MU_RUN_TEST(test_sllCreate_initialValues);
+}
+
 int main(){
-	
+	MU_RUN_SUITE(suite_sllCreate);
 	MU_REPORT();
 }
