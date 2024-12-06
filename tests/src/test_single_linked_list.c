105efce95f6a7f06f9268d5e23f508ae9305745c
Dimitris Papadimitriou <dimipapr@gmail.com>
2024-12-06 14:41:50 +0200

    renamed pointer variables to start with p

--- tests/src/test_single_linked_list.c
+++ tests/src/test_single_linked_list.c
@@ -4,20 +4,20 @@
 
 //sllCreate
 MU_TEST(test_sllCreate_badInput){
-	SingleLinkedList *list = sllCreate(0);
-	mu_assert(list==NULL,"sllCreate should return NULL with data_size=0");
-	list = sllCreate(-1);
-	mu_assert(list==NULL,"sllCreate should return NULL with data_size=0");
-	list = sllCreate(SLL_NODE_DATA_BYTES_MAX+1);
-	mu_assert(list==NULL,"sllCreate should return NULL with data_size>SLL_NODE_DATA_BYTES_MAX");
+	SingleLinkedList *pList = sllCreate(0);
+	mu_assert(pList==NULL,"sllCreate should return NULL with data_size=0");
+	pList = sllCreate(-1);
+	mu_assert(pList==NULL,"sllCreate should return NULL with data_size=0");
+	pList = sllCreate(SLL_NODE_DATA_BYTES_MAX+1);
+	mu_assert(pList==NULL,"sllCreate should return NULL with data_size>SLL_NODE_DATA_BYTES_MAX");
 }
 MU_TEST(test_sllCreate_initialValues){
 	int dataSize = sizeof(int);
-	SingleLinkedList *list = sllCreate(dataSize);
-	mu_assert(list, "Unexpected NULL from sllCreate");
-	mu_assert(list->pHead == NULL, "list->pHead should initialize to NULL");
-	mu_assert(list->dataSize == dataSize, "list->dataSize bad initialization");
-	sllDestroy(list);
+	SingleLinkedList *pList = sllCreate(dataSize);
+	mu_assert(pList, "Unexpected NULL from sllCreate");
+	mu_assert(pList->pHead == NULL, "list->pHead should initialize to NULL");
+	mu_assert(pList->dataSize == dataSize, "list->dataSize bad initialization");
+	sllDestroy(pList);
 }
 MU_TEST_SUITE(suite_sllCreate){
 	MU_RUN_TEST(test_sllCreate_badInput);
@@ -26,28 +26,28 @@ MU_TEST_SUITE(suite_sllCreate){
 //sllNodeCreate
 MU_TEST(test_sllNodeCreate_badInput){
 	int dataSize = sizeof(int);
-	SingleLinkedList *list = sllCreate(dataSize);
-	mu_check(list);
+	SingleLinkedList *pList = sllCreate(dataSize);
+	mu_check(pList);
 	int data=10;
-	SLLNode *node = sllNodeCreate(NULL,&data);
-	mu_assert(node==NULL,"sllNodeCreate should return NULL with NULL pList input");
-	node = sllNodeCreate(list,NULL);
-	mu_assert(node==NULL,"sllNodeCreate should return NULL with NULL pData input");
-	node = sllNodeCreate(NULL,NULL);
-	mu_assert(node==NULL,"sllNodeCreate should return NULL with NULL plist and pData input");
-	sllDestroy(list);
+	SLLNode *pNode = sllNodeCreate(NULL,&data);
+	mu_assert(pNode==NULL,"sllNodeCreate should return NULL with NULL pList input");
+	pNode = sllNodeCreate(pList,NULL);
+	mu_assert(pNode==NULL,"sllNodeCreate should return NULL with NULL pData input");
+	pNode = sllNodeCreate(NULL,NULL);
+	mu_assert(pNode==NULL,"sllNodeCreate should return NULL with NULL plist and pData input");
+	sllDestroy(pList);
 }
 MU_TEST(test_sllNodeCreate_initialValues){
 	int data_size = sizeof(int);
-	SingleLinkedList *list = sllCreate(data_size);
-	mu_check(list);
+	SingleLinkedList *pList = sllCreate(data_size);
+	mu_check(pList);
 	int data=10;
-	SLLNode *node = sllNodeCreate(list, &data);
-	mu_assert(node,"Unexpected NULL returned from sllNodeCreate");
-	mu_assert(*(int*)(node->pData)==data, "Bad data copy after sllNodeCreate");
-	mu_assert(node->pNext == NULL, "node->pNext initial value should be NULL");
-	sllNodeDestroy(node);
-	sllDestroy(list);
+	SLLNode *pNode = sllNodeCreate(pList, &data);
+	mu_assert(pNode,"Unexpected NULL returned from sllNodeCreate");
+	mu_assert(*(int*)(pNode->pData)==data, "Bad data copy after sllNodeCreate");
+	mu_assert(pNode->pNext == NULL, "node->pNext initial value should be NULL");
+	sllNodeDestroy(pNode);
+	sllDestroy(pList);
 }
 MU_TEST_SUITE(suite_sllNodeCreate){
 	MU_RUN_TEST(test_sllNodeCreate_badInput);
@@ -56,29 +56,29 @@ MU_TEST_SUITE(suite_sllNodeCreate){
 //sllInsertAtHead
 MU_TEST(test_sllInsertAtHead_badInput){
 	int dataSize = sizeof(int);
-	SingleLinkedList *list = sllCreate(dataSize);
-	mu_check(list);
+	SingleLinkedList *pList = sllCreate(dataSize);
+	mu_check(pList);
 	int data=10;
 	mu_assert(sllInsertAtHead(NULL,&data) == NULL, "sllInsertAtHead should return NULL with NULL list input");
-	mu_assert(sllInsertAtHead(list,NULL) == NULL, "sllInsertAtHead should return NULL with NULL data input");
+	mu_assert(sllInsertAtHead(pList,NULL) == NULL, "sllInsertAtHead should return NULL with NULL data input");
 	mu_assert(sllInsertAtHead(NULL,NULL) == NULL, "sllInsertAtHead should return NULL with NULL list and data input");
-	sllDestroy(list);
+	sllDestroy(pList);
 }
 MU_TEST(test_sllInsertAtHead_insertFiftyNodes){
-	SingleLinkedList *list = sllCreate(sizeof(int));
-	mu_check(list);
+	SingleLinkedList *pList = sllCreate(sizeof(int));
+	mu_check(pList);
 	int data=10;
 	for(int i=0;i<50;i++){
-		sllInsertAtHead(list,&data);		
+		sllInsertAtHead(pList,&data);		
 	}
-	mu_assert(sllLength(list) == 50, "Bad list count");
-	while(list->pHead){
-		SLLNode *tmp = list->pHead;
-		list->pHead = list->pHead->pNext;
+	mu_assert(sllLength(pList) == 50, "Bad list count");
+	while(pList->pHead){
+		SLLNode *tmp = pList->pHead;
+		pList->pHead = pList->pHead->pNext;
 		sllNodeDestroy(tmp);
 	}
-	mu_assert(sllLength(list)==0, "List should be empty now");
-	sllDestroy(list);
+	mu_assert(sllLength(pList)==0, "List should be empty now");
+	sllDestroy(pList);
 }
 MU_TEST_SUITE(suite_sllInsertAtHead){
 	MU_RUN_TEST(test_sllInsertAtHead_badInput);
